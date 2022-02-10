6822c4eef88bb163fab256e78120dbb33de94286
Ruven Salamon <ruven@credix.finance>
2022-02-10 09:26:47 +0100

    refactor: removes need for filesystem keypair during testing

--- test/Borrower.test.ts
+++ test/Borrower.test.ts
@@ -1,18 +1,10 @@
-import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
-import { Connection } from "@solana/web3.js";
-import { CredixClient, CredixClientConfig } from "index";
-import { testProgramId } from "./util";
+import { testClient } from "./util";
 
 describe("Borrower", () => {
-	const connection = new Connection("http://127.0.0.1:8899");
-	const credixClientConfig: CredixClientConfig = {
-		programId: testProgramId,
-		confirmOptions: { commitment: "finalized" },
-	};
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	const client = new CredixClient(connection, NodeWallet.local(), credixClientConfig);
+	const client = testClient;
 
 	it("returns the correct market", () => {
 		throw Error("not implemented");
